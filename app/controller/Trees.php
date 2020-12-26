2396fd09c182615a11a7ad7def4f272198232abe
lDemodexl <38405306+lDemodexl@users.noreply.github.com>
2020-12-26 10:51:16 +0200

    Recursive deletion
    add recursive deletion
    add some comments
    remove some code no need in

--- app/controller/Trees.php
+++ app/controller/Trees.php
@@ -4,10 +4,8 @@ class Trees extends Controller{
 		$this->treeModel = $this->model('Tree');
 	}
 	
+	//add element to tree
 	public function add(){
-
-		$data = [];
-		$errors = array();
 		
 		if( $_SERVER['REQUEST_METHOD'] == 'POST' ){
 			if( !empty($_POST) ){
@@ -34,22 +32,36 @@ class Trees extends Controller{
 		}else{
 			redirect('');
 		}
-	}
-	
+	}	
 	
 	//Delete the element
 	public function delete(){
 		if( $_SERVER['REQUEST_METHOD'] == 'POST' ){
             //Sanitize Post
-            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+			$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+			if( $this->treeModel->isParent($_POST['id']) ){
+				$this->deleteRecursive($_POST['id']);
+			}
 			if( $this->treeModel->deleteElement($_POST['id']) ){
 				return $this->createTree();		
 			}
 		}else{
 			redirect('');
 		}
-    }
-    
+	}
+	
+	//Delete element childs
+	function deleteRecursive($id){
+		$childs = $this->treeModel->getChilds($id);
+		foreach( $childs as $child ){
+			if( $this->treeModel->isParent($child->id) ){
+				$this->deleteRecursive($child->id);
+			}
+			$this->treeModel->deleteElement($child->id);
+		}
+	}
+	
+	//build tree and return new html
     function createTree(){
         $new_tree = $this->treeModel->getTree();
         $new_tree = $this->parseTree($new_tree);
@@ -60,6 +72,7 @@ class Trees extends Controller{
         return $this->view('pages/partials/tree', $data);
     }
 
+	//collect all records to tree structure
     function parseTree($tree, $parentID = '' ){
         $parsed_tree = [];
         foreach( $tree as $key=>$tree_element ){
